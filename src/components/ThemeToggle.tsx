dc10ff662fdcf408b437dd27986fd3e3828a81fc
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-08-11 11:32:44 +0300

    Convert px to rem.

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -14,8 +14,8 @@ export const ThemeToggle = () => {
     {
       toggleBg: C.LIGHTER_GREY,
       bg: C.WHITE,
-      top: '5px',
-      left: '5px',
+      top: '0.3125rem',
+      left: '0.3125rem',
       message: 'Light',
       Icon: IconLightMode,
       iconColor: C.GREY,
@@ -23,7 +23,7 @@ export const ThemeToggle = () => {
     {
       toggleBg: C.DARKER_GREY,
       bg: C.BLACK,
-      top: '43px',
+      top: '2.6875rem',
       left: '50%',
       message: 'Dark',
       Icon: IconDarkMode,
@@ -35,66 +35,69 @@ export const ThemeToggle = () => {
       overflow="hidden"
       position="relative"
       bg={values.toggleBg}
-      borderRadius="5px"
+      borderRadius="0.3125rem"
       justifyContent="center"
       alignItems="center"
-      w={{ base: '44px', sm: '14.5rem' }}
-      h={{ base: '84px', sm: '2.125rem' }}
-      m={{ base: '6px', sm: 'auto' }}
+      w={{ base: '2.75rem', sm: '14.5rem' }}
+      h={{ base: '5.25rem', sm: '2.125rem' }}
+      m={{ base: '0.375rem', sm: 'auto' }}
       flexDirection={{ base: 'column', sm: 'row' }}
       onClick={toggleColorMode}
       cursor="pointer"
-      fontSize="14px"
+      fontSize="0.875rem"
     >
       <Flex
-        p="6px"
+        p="0.375rem"
         zIndex="100"
         position="absolute"
         transition="top 0.3s ease-out, left 0.2s ease-out, background 0.16s ease-out"
         top={{ base: values.top, sm: '4px' }}
         left={{ base: 'inherit', sm: values.left }}
-        w={{ base: '34px', sm: 'calc(50% - 6px)' }}
-        h={{ base: '36px', sm: '26px' }}
+        w={{ base: '2.125rem', sm: 'calc(50% - 6px)' }}
+        h={{ base: '2.25rem', sm: '1.625rem' }}
         bg={values.bg}
-        borderRadius="5px"
+        borderRadius="0.3125rem"
         justifyContent="center"
         alignItems="center"
       >
         <values.Icon
           fill={values.iconColor}
-          style={{ width: '18px', height: '18px' }}
+          style={{ width: '1.125rem', height: '1.125rem' }}
         />
         <chakra.span display={{ base: 'none', sm: 'inline-block' }} ml="0.4rem">
           {values.message}
         </chakra.span>
       </Flex>
       <Flex
-        h={{ base: '36px', sm: '26px' }}
-        m={{ base: '0', sm: '6px' }}
-        mr={{ base: '0', sm: '3px' }}
-        w={{ base: '36px', sm: '50%' }}
+        h={{ base: '2.25rem', sm: '1.625rem' }}
+        m={{ base: '0', sm: '0.375rem' }}
+        mr={{ base: '0', sm: '0.1875rem' }}
+        w={{ base: '2.25rem', sm: '50%' }}
         textAlign="center"
         justifyContent="center"
         alignItems="center"
       >
         <IconLightMode
           fill={C.GREY}
-          style={{ width: '18px', height: '18px' }}
+          style={{ width: '1.125rem', height: '1.125rem' }}
         />
         <chakra.span display={{ base: 'none', sm: 'inline-block' }} ml="0.4rem">
           Light
         </chakra.span>
       </Flex>
       <Flex
-        h={{ base: '36px', sm: '26px' }}
-        m={{ base: '0', sm: '6px' }}
-        ml={{ base: '0', sm: '3px' }}
-        w={{ base: '36px', sm: '50%' }}
+        h={{ base: '2.25rem', sm: '1.625rem' }}
+        m={{ base: '0', sm: '0.375rem' }}
+        ml={{ base: '0', sm: '0.1875rem' }}
+        w={{ base: '2.25rem', sm: '50%' }}
         textAlign="center"
         justifyContent="center"
         alignItems="center"
       >
-        <IconDarkMode fill={C.GREY} style={{ width: '18px', height: '18px' }} />
+        <IconDarkMode
+          fill={C.GREY}
+          style={{ width: '1.125rem', height: '1.125rem' }}
+        />
         <chakra.span display={{ base: 'none', sm: 'inline-block' }} ml="0.4rem">
           Dark
         </chakra.span>
