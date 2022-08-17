129cbe14b1904611d727abdf13d0a2bb453b6301
Aliaksandr Zhurauski <39185158+azhurauski-sc@users.noreply.github.com>
2022-08-17 10:29:05 +0300

    (IRO-2469) Update item without header layout. (#23)

--- src/routes/AddressDetails/AddressTransactions.tsx
+++ src/routes/AddressDetails/AddressTransactions.tsx
@@ -69,10 +69,11 @@ const AddressTransactions = () => {
               key: 'action',
               label: 'Action',
               render: address => (
-                <Flex align="center" gap="12px">
+                <Flex align="center" position="relative">
                   <Flex
-                    w="26px"
-                    h="26px"
+                    w="1.625rem"
+                    h="1.625rem"
+                    position="absolute"
                     borderRadius="50%"
                     align="center"
                     justify="center"
@@ -82,36 +83,41 @@ const AddressTransactions = () => {
                       {address.action === 'Send' ? <Send /> : <Receive />}
                     </Icon>
                   </Flex>
-                  {address.action}
+                  <chakra.h5 ml="2.375rem">{address.action}</chakra.h5>
                 </Flex>
               ),
             },
             {
               key: 'iron',
               label: '$IRON',
-              render: address => <Box>{address.iron}</Box>,
+              render: address => <h5>{address.iron}</h5>,
             },
             {
               key: 'to',
               label: 'To',
-              render: address => <Flex>{address.to}</Flex>,
+              render: address => <h5>{address.to}</h5>,
             },
             {
               key: 'date',
               label: 'Date',
-              render: address => <Flex>{address.date}</Flex>,
+              render: address => <h5>{address.date}</h5>,
             },
             {
               key: 'memo',
               label: 'Memo',
-              render: address => <Flex>{address.memo}</Flex>,
+              render: address => <h5>{address.memo}</h5>,
             },
             {
               key: 'actions',
-              label: <>&nbsp;</>,
+              label: '',
+              WrapperProps: {
+                display: 'flex',
+              },
+              ItemProps: {
+                justifyContent: 'flex-end',
+              },
               render: () => (
                 <Link
-                  display="flex"
                   sx={{
                     color: NAMED_COLORS.LIGHT_BLUE,
                     _hover: {
@@ -119,8 +125,10 @@ const AddressTransactions = () => {
                     },
                   }}
                 >
-                  <chakra.h5 mr="0.3125rem">View Details</chakra.h5>
-                  <Caret />
+                  <Flex>
+                    <chakra.h5 mr="0.3125rem">View Details</chakra.h5>
+                    <Caret />
+                  </Flex>
                 </Link>
               ),
             },
