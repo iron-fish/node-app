7e7c497524592b6dac5785e6e32fcc11066bf186
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-08-17 17:49:56 +0300

    (IRO-2371) Added copy to clipboard icon and update navigation.

--- src/routes/Accounts/AccountPreview.tsx
+++ src/routes/Accounts/AccountPreview.tsx
@@ -8,12 +8,14 @@ import {
   IconButton,
   NAMED_COLORS,
   chakra,
+  CopyValueToClipboard,
 } from '@ironfish/ui-kit'
 import Send from 'Svgx/send'
 import Receive from 'Svgx/receive'
 import Caret from 'Svgx/caret-icon'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '..'
+import { truncateHash } from 'Utils/hash'
 
 export interface AccountPreviewProps {
   order: number
@@ -78,113 +80,133 @@ const AccountPreview: FC<AccountPreviewProps> = ({
   name,
   balance = 0,
   address,
-}) => (
-  <Flex
-    p="0.75rem"
-    my="0.5rem"
-    border="0.063rem solid"
-    borderColor={NAMED_COLORS.LIGHT_GREY}
-    borderRadius="0.25rem"
-    cursor="pointer"
-    as={Link}
-    to={ROUTES.ACCOUNT}
-    state={{ accountId: address }}
-    sx={{
-      transition: '0.3s',
-      bg: NAMED_COLORS.WHITE,
-      borderColor: NAMED_COLORS.LIGHT_GREY,
-      '.chakra-ui-dark &': {
-        bg: NAMED_COLORS.DARKER_GREY,
-        borderColor: NAMED_COLORS.DARK_GREY,
-      },
-      boxShadow: '0px 0.25rem 0.6875rem rgba(0, 0, 0, 0.04)',
-      zIndex: 0,
-    }}
-  >
+}) => {
+  const navigate = useNavigate()
+  return (
     <Flex
-      w={{ base: '6rem', md: '10.375rem' }}
-      h="6rem"
-      mr={{
-        base: '1.25rem',
-        sm: '1.25rem',
-        lg: '2.75rem',
-      }}
-      bg={`linear-gradient(89.56deg, ${
-        ORDER_COLOR[order % ORDER_COLOR.length].from
-      } 0.38%, ${ORDER_COLOR[order % ORDER_COLOR.length].to} 99.64%)`}
-      borderColor={NAMED_COLORS.BLACK}
-      borderRadius="0.25rem"
+      p="0.75rem"
+      my="0.5rem"
       border="0.063rem solid"
-      float="right"
-      alignItems="center"
-      justifyContent="center"
-      _before={{
-        w: { base: '6rem', md: '10.375rem' },
-        h: '6rem',
-        position: 'absolute',
-        content: `""`,
-        borderColor: NAMED_COLORS.BLACK,
-        borderRadius: '0.25rem',
-        border: '0.063rem solid',
-        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${
-          ORDER_COLOR[order % ORDER_COLOR.length].to
-        } 99.64%)`,
-        mr: '-0.25rem',
-        mb: '-0.25rem',
-        mt: '0.25rem',
-        ml: '0.25rem',
-        zIndex: -1,
+      borderColor={NAMED_COLORS.LIGHT_GREY}
+      borderRadius="0.25rem"
+      cursor="pointer"
+      onClick={() =>
+        navigate(ROUTES.ACCOUNT, { state: { accountId: address } })
+      }
+      sx={{
+        transition: '0.3s',
+        bg: NAMED_COLORS.WHITE,
+        borderColor: NAMED_COLORS.LIGHT_GREY,
+        '.chakra-ui-dark &': {
+          bg: NAMED_COLORS.DARKER_GREY,
+          borderColor: NAMED_COLORS.DARK_GREY,
+        },
+        boxShadow: '0px 0.25rem 0.6875rem rgba(0, 0, 0, 0.04)',
+        zIndex: 0,
       }}
     >
-      <HexFish style={{ height: '2rem' }} />
-    </Flex>
-    <Box>
-      <chakra.h5 pt="0.25rem">{name}</chakra.h5>
-      <chakra.h3 p="4px 0">{balance} $IRON</chakra.h3>
-      <h5 style={{ color: NAMED_COLORS.GREY }}>{address}</h5>
-    </Box>
-    <Flex ml="auto" alignSelf="center">
-      <Flex direction="column" gap="0.75rem">
-        <Button
-          variant="primary"
-          borderRadius="4rem"
-          mr="1rem"
-          onClick={e => {
-            // required to prevent triggering card click event
-            e.stopPropagation()
-          }}
-          leftIcon={
-            <Icon height={8}>
-              <Send fill="currentColor" />
-            </Icon>
-          }
-        >
-          <h5>Send</h5>
-        </Button>
-        <Button
-          variant="primary"
-          borderRadius="4rem"
-          mr="1rem"
-          onClick={e => {
-            // required to prevent triggering card click event
-            e.stopPropagation()
-          }}
-          leftIcon={
-            <Icon height={8}>
-              <Receive fill="currentColor" />
-            </Icon>
-          }
-        >
-          <h5>Receive</h5>
-        </Button>
+      <Flex
+        w={{ base: '6rem', md: '10.375rem' }}
+        h="6rem"
+        mr={{
+          base: '1.25rem',
+          sm: '1.25rem',
+          lg: '2.75rem',
+        }}
+        bg={`linear-gradient(89.56deg, ${
+          ORDER_COLOR[order % ORDER_COLOR.length].from
+        } 0.38%, ${ORDER_COLOR[order % ORDER_COLOR.length].to} 99.64%)`}
+        borderColor={NAMED_COLORS.BLACK}
+        borderRadius="0.25rem"
+        border="0.063rem solid"
+        float="right"
+        alignItems="center"
+        justifyContent="center"
+        _before={{
+          w: { base: '6rem', md: '10.375rem' },
+          h: '6rem',
+          position: 'absolute',
+          content: `""`,
+          borderColor: NAMED_COLORS.BLACK,
+          borderRadius: '0.25rem',
+          border: '0.063rem solid',
+          bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${
+            ORDER_COLOR[order % ORDER_COLOR.length].to
+          } 99.64%)`,
+          mr: '-0.25rem',
+          mb: '-0.25rem',
+          mt: '0.25rem',
+          ml: '0.25rem',
+          zIndex: -1,
+        }}
+      >
+        <HexFish style={{ height: '2rem' }} />
       </Flex>
-      <IconButton
-        alignSelf="center"
-        aria-label="account-details"
-        variant="ghost"
-        icon={<Caret />}
-      />
+      <Box>
+        <chakra.h5 pt="0.25rem">{name}</chakra.h5>
+        <chakra.h3 p="4px 0">{balance} $IRON</chakra.h3>
+        <CopyValueToClipboard
+          containerProps={{
+            color: NAMED_COLORS.GREY,
+          }}
+          iconButtonProps={{
+            justifyContent: 'none',
+            minW: '0.75rem',
+            'aria-label': 'copy',
+          }}
+          labelProps={{
+            mr: '0.5rem',
+          }}
+          value={address}
+          label={<chakra.h5>{truncateHash(address, 3)}</chakra.h5>}
+          copyTooltipText="Copy to clipboard"
+          copiedTooltipText="Copied"
+        />
+      </Box>
+      <Flex ml="auto" alignSelf="center">
+        <Flex direction="column" gap="0.75rem">
+          <Button
+            variant="primary"
+            borderRadius="4rem"
+            mr="1rem"
+            onClick={e => {
+              // required to prevent triggering card click event
+              e.stopPropagation()
+            }}
+            leftIcon={
+              <Icon height={8}>
+                <Send fill="currentColor" />
+              </Icon>
+            }
+          >
+            <h5>Send</h5>
+          </Button>
+          <Button
+            variant="primary"
+            borderRadius="4rem"
+            mr="1rem"
+            onClick={e => {
+              // required to prevent triggering card click event
+              e.stopPropagation()
+            }}
+            leftIcon={
+              <Icon height={8}>
+                <Receive fill="currentColor" />
+              </Icon>
+            }
+          >
+            <h5>Receive</h5>
+          </Button>
+        </Flex>
+        <IconButton
+          alignSelf="center"
+          aria-label="account-details"
+          variant="ghost"
+          icon={<Caret />}
+        />
+      </Flex>
     </Flex>
-  </Flex>
-)
+  )
+}
+
 export default AccountPreview
--- src/routes/Accounts/Accounts.tsx
+++ src/routes/Accounts/Accounts.tsx
@@ -17,62 +17,62 @@ const DEMO_DATA = [
   {
     name: 'Primary Account',
     balance: 8.456,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Secondary Account',
     balance: 1.944,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 3',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 4',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 5',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 6',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 7',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 8',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 9',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 10',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 11',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
   {
     name: 'Account 12',
     balance: 56,
-    address: '456t...543d...53d5',
+    address: '456tenft893ntw5v780ntq304wnv5t370q8nt553d5',
   },
 ]
 
