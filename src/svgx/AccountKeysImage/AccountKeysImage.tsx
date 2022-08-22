ac2e49b0262113fe683d0b10eb734d820f55c87d
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-08-22 16:12:41 +0300

    Fix remark.

--- src/svgx/AccountKeysImage/AccountKeysImage.tsx
+++ src/svgx/AccountKeysImage/AccountKeysImage.tsx
@@ -1,14 +1,13 @@
-import { IconProps, useColorMode } from '@ironfish/ui-kit'
+import { IconProps, useColorModeValue } from '@ironfish/ui-kit'
 import AccountKeysImageDark from './AccountKeysImageDark'
 import AccountKeysImageLight from './AccountKeysImageLight'
 
 const AccountKeysImage = (props: IconProps) => {
-  const { colorMode } = useColorMode()
-  return colorMode === 'light' ? (
-    <AccountKeysImageLight {...props} />
-  ) : (
-    <AccountKeysImageDark {...props} />
+  const KeyImage = useColorModeValue(
+    AccountKeysImageLight,
+    AccountKeysImageDark
   )
+  return <KeyImage {...props} />
 }
 
 export default AccountKeysImage
