c4830ec2e388d5fa32145ee3422be39b334e7c88
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-08-31 13:40:14 +0300

    Changed button text on address book.

--- src/routes/AddressBook.tsx
+++ src/routes/AddressBook.tsx
@@ -162,7 +162,7 @@ const AddressBook: FC = () => {
             borderRadius="4rem"
             variant="secondary"
           >
-            <h5>Import Account</h5>
+            <h5>Add Contact</h5>
           </Button>
         </Flex>
       </Flex>
