c2a28d716ae598ac4cb436f5a250b55bc4b80793
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-09-13 15:17:01 +0300

    Update demoData manager.

--- src/data/DemoDataManager.ts
+++ src/data/DemoDataManager.ts
@@ -13,7 +13,7 @@ import {
 import { Contact } from './types/Contact'
 import { Transaction } from './types/Transaction'
 
-class DemoDataManager {
+export default class DemoDataManager {
   accounts: DemoAccountsManager
   transactions: DemoTransactionsManager
   addressBook: DemoAddressBookManager
@@ -28,7 +28,7 @@ class DemoDataManager {
     this.node = new DemoNodeManager()
   }
 
-  createAccount(
+  createAccount = (
     name: string,
     mnemonicPhrase: [
       string,
@@ -48,11 +48,11 @@ class DemoDataManager {
     return this.accounts.create(name, mnemonicPhrase)
   }
 
-  generateMnemonic(): Promise<string[]> {
+  generateMnemonic = (): Promise<string[]> => {
     return this.accounts.generateMnemonicPhrase()
   }
 
-  importAccountBySpendingKey(spendingKey: string): Promise<string> {
+  importAccountBySpendingKey = (spendingKey: string): Promise<string> => {
     return this.accounts.importBySpendingKey(spendingKey)
   }
 
@@ -75,57 +75,60 @@ class DemoDataManager {
     return this.accounts.importByMnemonicPhrase(mnemonicPhrase)
   }
 
-  importAccountByFile(file: File): Promise<string> {
+  importAccountByFile = (file: File): Promise<string> => {
     return this.accounts.importByFile(file)
   }
 
-  getAccounts(searchTerm?: string): Promise<Account[]> {
+  getAccounts = (searchTerm?: string): Promise<Account[]> => {
     return this.accounts.list(searchTerm || '')
   }
 
-  getAccount(accountId: string): Promise<Account> {
+  getAccount = (accountId: string): Promise<Account> => {
     return this.accounts.findById(accountId)
   }
 
-  getAccountKeys(accountId: string): Promise<AccountKeys> {
+  getAccountKeys = (accountId: string): Promise<AccountKeys> => {
     return this.accounts.keys(accountId)
   }
 
-  getAccountSettings(accountId: string): Promise<AccountSettings> {
+  getAccountSettings = (accountId: string): Promise<AccountSettings> => {
     return this.accounts.settings(accountId)
   }
 
-  findTransactionsByAddress(
+  findTransactionsByAddress = (
     address: string,
     search?: string,
     sort?: 'asc' | 'desc'
-  ): Promise<Transaction[]> {
+  ): Promise<Transaction[]> => {
     return this.transactions.findByAddress(address, search, sort)
   }
 
-  calculateFee(amount: number): Promise<number> {
+  calculateFee = (amount: number): Promise<number> => {
     return this.transactions.calculateFee(amount)
   }
 
-  sendTransaction(
+  sendTransaction = (
     from: string,
     to: string,
     amount: number,
     memo: string,
     fee: number
-  ): Promise<string> {
+  ): Promise<string> => {
     return this.transactions.send(from, to, amount, memo, fee)
   }
 
-  getAddressBook(search: string, sort?: 'asc' | 'desc'): Promise<Contact[]> {
+  getAddressBook = (
+    search: string,
+    sort?: 'asc' | 'desc'
+  ): Promise<Contact[]> => {
     return this.addressBook.list(search, sort)
   }
 
-  getContact(identity: string): Promise<Contact> {
+  getContact = (identity: string): Promise<Contact> => {
     return this.addressBook.findById(identity)
   }
 
-  updateContact(
+  updateContact = (
     identity: string,
     name: string,
     address: string
@@ -133,41 +136,41 @@ class DemoDataManager {
     return this.addressBook.update(identity, name, address)
   }
 
-  deleteContact(identity: string): Promise<boolean> {
+  deleteContact = (identity: string): Promise<boolean> => {
     return this.addressBook.delete(identity)
   }
 
-  addContact(name: string, address: string): Promise<string> {
+  addContact = (name: string, address: string): Promise<string> => {
     return this.addressBook.add(name, address)
   }
 
-  getAccountMinerStatus(accountId: string): Promise<AccountMinerStatus> {
+  getAccountMinerStatus = (accountId: string): Promise<AccountMinerStatus> => {
     return this.miner.status(accountId)
   }
 
-  getAccountMinerStatistic(accountId: string): Promise<AccountMinerStatistic> {
+  getAccountMinerStatistic = (
+    accountId: string
+  ): Promise<AccountMinerStatistic> => {
     return this.miner.statistic(accountId)
   }
 
-  getAccountMinerSpeed(accountId: string): Promise<AccountMinerSpeed> {
+  getAccountMinerSpeed = (accountId: string): Promise<AccountMinerSpeed> => {
     return this.miner.speed(accountId)
   }
 
-  startMining(accountId: string): Promise<boolean> {
+  startMining = (accountId: string): Promise<boolean> => {
     return this.miner.start(accountId)
   }
 
-  stopMining(accountId: string): Promise<boolean> {
+  stopMining = (accountId: string): Promise<boolean> => {
     return this.miner.stop(accountId)
   }
 
-  getNodeStatus(): Promise<GetStatusResponse> {
+  getNodeStatus = (): Promise<GetStatusResponse> => {
     return this.node.status()
   }
 
-  getNodePeers(): Promise<PeerResponse[]> {
+  getNodePeers = (): Promise<PeerResponse[]> => {
     return this.node.peers()
   }
 }
-
-export default DemoDataManager
