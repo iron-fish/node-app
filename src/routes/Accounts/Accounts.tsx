5129e75ffc450e555336fcf4abe858619d4e8e1c
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-10-03 15:36:47 +0300

    Added icons.

--- src/routes/Accounts/Accounts.tsx
+++ src/routes/Accounts/Accounts.tsx
@@ -6,12 +6,7 @@ import {
   chakra,
   NAMED_COLORS,
   useColorModeValue,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
   useBreakpointValue,
-  Icon,
   ButtonsGroup,
 } from '@ironfish/ui-kit'
 import IconAdd from '@ironfish/ui-kit/dist/svgx/icon-add'
@@ -23,11 +18,6 @@ import ImportAccount from 'Routes/Onboarding/ImportAccount'
 import CreateAccount from 'Routes/Onboarding/CreateAccount'
 import SortType from 'Types/SortType'
 
-const DemoThreeDotsIcon: FC = () => (
-  <Icon w={7} viewBox="0 0 28 28" style={{ marginBottom: 5, marginRight: 3 }}>
-    <path d="M3.968,12.061C1.775,12.061,0,13.835,0,16.027c0,2.192,1.773,3.967,3.968,3.967c2.189,0,3.966-1.772,3.966-3.967   C7.934,13.835,6.157,12.061,3.968,12.061z M16.233,12.061c-2.188,0-3.968,1.773-3.968,3.965c0,2.192,1.778,3.967,3.968,3.967   s3.97-1.772,3.97-3.967C20.201,13.835,18.423,12.061,16.233,12.061z M28.09,12.061c-2.192,0-3.969,1.774-3.969,3.967   c0,2.19,1.774,3.965,3.969,3.965c2.188,0,3.965-1.772,3.965-3.965S30.278,12.061,28.09,12.061z" />
-  </Icon>
-)
 interface ActionButtonsProps {
   showCreate: (show: boolean) => void
   showImport: (show: boolean) => void
@@ -38,12 +28,22 @@ const ActionButtons: FC<ActionButtonsProps> = ({ showCreate, showImport }) => {
     () => [
       {
         key: 'accounts-view-create',
-        label: 'Create Account',
+        label: (
+          <Flex alignItems="center">
+            <IconAdd mr="8px" w="1rem" h="1rem" mb="2px" />
+            Create Account
+          </Flex>
+        ),
         onClick: () => showCreate(true),
       },
       {
         key: 'accounts-view-import',
-        label: 'Import Account',
+        label: (
+          <Flex alignItems="center">
+            <IconAdd mr="8px" w="1rem" h="1rem" mb="2px" />
+            Import Account
+          </Flex>
+        ),
         onClick: () => showImport(true),
       },
     ],
@@ -57,7 +57,6 @@ const ActionButtons: FC<ActionButtonsProps> = ({ showCreate, showImport }) => {
       {buttons.map(({ key, label, onClick }) => (
         <Button
           key={key}
-          leftIcon={<IconAdd mr="-0.25rem" />}
           borderRadius="4rem"
           variant="secondary"
           onClick={onClick}
