bfce4024a4fc19c79240ea5080b8c75edc274615
Nikita Demidovich <ndemidovich-sc@users.noreply.github.com>
2022-10-12 13:01:23 +0300

    IRO-2658 - Added local storage for Address book and Accounts settings (#68)

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const { copySync } = require('fs-extra')
+
+copySync(__dirname + '/types', __dirname + '/src/types', { overwrite: true })
 
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
