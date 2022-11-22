027aa06a6e5896f1fedf80485e274df449a02596
Aliaksandr Zhurauski <39185158+azhurauski-sc@users.noreply.github.com>
2022-11-22 11:51:32 +0300

    Convert Ore to Iron and formate. (#112)

--- src/components/AccountBalance.tsx
+++ src/components/AccountBalance.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react'
 import useAccountBalance from 'Hooks/accounts/useAccountBalance'
 import { Skeleton, SkeletonProps } from '@ironfish/ui-kit'
 import Balance from 'Types/AccountBalance'
+import { CurrencyUtils } from '@ironfish/sdk/build/src/utils/currency'
 
 const AccountBalance: FC<{
   accountId: string
@@ -10,7 +11,8 @@ const AccountBalance: FC<{
 }> = ({
   accountId,
   skeletonProps,
-  renderBalance = balance => balance?.confirmed?.toString() || 0,
+  renderBalance = balance =>
+    CurrencyUtils.renderIron(balance?.confirmed || '0'),
 }) => {
   const { loaded, data: balance } = useAccountBalance(accountId)
   return (
