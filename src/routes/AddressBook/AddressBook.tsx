8654d3e776cb23f4d212643a8b2d942f7d24883c
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-11-25 12:49:09 +0300

    Fix blank screen on search in address book.

--- src/routes/AddressBook/AddressBook.tsx
+++ src/routes/AddressBook/AddressBook.tsx
@@ -27,87 +27,6 @@ import AddContactModal from './AddContactModal'
 import { useDataSync } from 'Providers/DataSyncProvider'
 import { stringToColor } from 'Utils/stringToColor'
 
-const COLUMNS = [
-  {
-    key: 'contact',
-    label: 'Contact',
-    render: (contact: Contact) => (
-      <Flex alignItems="center">
-        <HexFishCircle mr="1rem" bg={stringToColor(contact._id, 73)} />
-        <h5>{contact.name}</h5>
-      </Flex>
-    ),
-  },
-  {
-    key: 'address',
-    label: 'Address',
-    render: (contact: Contact) => {
-      const addressLabel = useBreakpointValue({
-        base: truncateHash(contact.address, 2, 9),
-        md: truncateHash(contact.address, 2, 16),
-        lg: contact.address,
-      })
-      return (
-        <CopyValueToClipboard
-          iconButtonProps={{
-            justifyContent: 'none',
-            minW: '0.75rem',
-            color: NAMED_COLORS.GREY,
-          }}
-          labelProps={{
-            mr: '0.5rem',
-          }}
-          value={contact.address}
-          label={<chakra.h5>{addressLabel}</chakra.h5>}
-          copyTooltipText="Copy to clipboard"
-          copiedTooltipText="Copied"
-        />
-      )
-    },
-  },
-  {
-    key: 'actions',
-    label: '',
-    render: (contact: Contact) => {
-      const navigate = useNavigate()
-      const { loaded } = useDataSync()
-      return (
-        <Flex justify="flex-end" mr="-1.0625rem">
-          <Box
-            onClick={e => {
-              e.stopPropagation()
-              if (loaded) {
-                navigate(ROUTES.SEND, { state: { contactId: contact?._id } })
-              }
-            }}
-          >
-            <Button
-              leftIcon={
-                <Icon height={8}>
-                  <Send fill="currentColor" />
-                </Icon>
-              }
-              variant="primary"
-              borderRadius="4rem"
-              isDisabled={!loaded}
-              mr={{ base: '0.75rem', md: '1rem' }}
-            >
-              <h5>Send</h5>
-            </Button>
-          </Box>
-          <IconButton
-            aria-label="book-details"
-            variant="ghost"
-            icon={<Caret />}
-            _active={{ bg: 'none' }}
-            _hover={{ bg: 'none' }}
-          />
-        </Flex>
-      )
-    },
-  },
-]
-
 const AddContactButton: FC<{
   onAdd: (name: string, address: string) => Promise<void>
 }> = ({ onAdd }) => {
@@ -147,10 +66,16 @@ const AddressBook: FC = () => {
       caretColor: NAMED_COLORS.PALE_GREY,
     }
   )
+  const $getAddressLabel = useBreakpointValue({
+    base: address => truncateHash(address, 2, 9),
+    md: address => truncateHash(address, 2, 16),
+    lg: address => address,
+  })
 
   const [$searchTerm, $setSearchTerm] = useState('')
   const [$sortOrder, $setSortOrder] = useState<SortType>(SortType.ASC)
 
+  const { loaded: synced } = useDataSync()
   const [{ data: contacts, loaded }, addContact] = useAddressBook(
     $searchTerm,
     $sortOrder
@@ -197,7 +122,86 @@ const AddressBook: FC = () => {
             navigate(ROUTES.ADDRESS_BOOK + `/${contact._id}`)
           }
           data={loaded ? contacts : new Array(10).fill(null)}
-          columns={COLUMNS}
+          columns={[
+            {
+              key: 'contact',
+              label: 'Contact',
+              render: (contact: Contact) => (
+                <Flex alignItems="center">
+                  <HexFishCircle
+                    mr="1rem"
+                    bg={stringToColor(contact._id, 73)}
+                  />
+                  <h5>{contact.name}</h5>
+                </Flex>
+              ),
+            },
+            {
+              key: 'address',
+              label: 'Address',
+              render: (contact: Contact) => {
+                return (
+                  <CopyValueToClipboard
+                    iconButtonProps={{
+                      justifyContent: 'none',
+                      minW: '0.75rem',
+                      color: NAMED_COLORS.GREY,
+                    }}
+                    labelProps={{
+                      mr: '0.5rem',
+                    }}
+                    value={contact.address}
+                    label={
+                      <chakra.h5>{$getAddressLabel(contact.address)}</chakra.h5>
+                    }
+                    copyTooltipText="Copy to clipboard"
+                    copiedTooltipText="Copied"
+                  />
+                )
+              },
+            },
+            {
+              key: 'actions',
+              label: '',
+              render: (contact: Contact) => {
+                return (
+                  <Flex justify="flex-end" mr="-1.0625rem">
+                    <Box
+                      onClick={e => {
+                        e.stopPropagation()
+                        if (synced) {
+                          navigate(ROUTES.SEND, {
+                            state: { contactId: contact?._id },
+                          })
+                        }
+                      }}
+                    >
+                      <Button
+                        leftIcon={
+                          <Icon height={8}>
+                            <Send fill="currentColor" />
+                          </Icon>
+                        }
+                        variant="primary"
+                        borderRadius="4rem"
+                        isDisabled={!synced}
+                        mr={{ base: '0.75rem', md: '1rem' }}
+                      >
+                        <h5>Send</h5>
+                      </Button>
+                    </Box>
+                    <IconButton
+                      aria-label="book-details"
+                      variant="ghost"
+                      icon={<Caret />}
+                      _active={{ bg: 'none' }}
+                      _hover={{ bg: 'none' }}
+                    />
+                  </Flex>
+                )
+              },
+            },
+          ]}
           sx={{
             tr: {
               '[aria-label="book-details"]': {
