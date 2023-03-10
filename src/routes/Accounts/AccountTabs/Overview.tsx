5a4413cf9e37a6c088794eddc6fe93c364fe3fb2
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2023-03-10 13:47:13 +0300

    Move overview balance to separate component to prevent transactions refetching.

--- src/routes/Accounts/AccountTabs/Overview.tsx
+++ src/routes/Accounts/AccountTabs/Overview.tsx
@@ -47,6 +47,7 @@ const SearchTransactions: FC<SearchTransactionsProps> = ({ address }) => {
     loaded,
     actions: { reload },
   } = useTransactions(address, deferredSearchTerm, $sortOrder)
+  const trxLoaded = useDeferredValue(loaded)
   const [, setTransactionsState] = useState([])
   const toast = useIronToast({
     containerStyle: {
@@ -56,12 +57,12 @@ const SearchTransactions: FC<SearchTransactionsProps> = ({ address }) => {
 
   useEffect(() => {
     let interval: NodeJS.Timer
-    if (loaded) {
+    if (trxLoaded) {
       interval = setInterval(reload, 5000)
     }
 
     return () => interval && clearInterval(interval)
-  }, [loaded])
+  }, [trxLoaded])
 
   useEffect(() => {
     setTransactionsState(prevTransactions => {
@@ -112,9 +113,11 @@ const SearchTransactions: FC<SearchTransactionsProps> = ({ address }) => {
       }
       return transactions.map(({ hash, status }) => ({ hash, status }))
     })
-  }, [loaded])
+  }, [trxLoaded])
 
-  return loaded && transactions?.length === 0 && !deferredSearchTerm ? null : (
+  return trxLoaded &&
+    transactions?.length === 0 &&
+    !deferredSearchTerm ? null : (
     <>
       <Box>
         <chakra.h3 pb="1rem">Transactions</chakra.h3>
@@ -232,10 +235,7 @@ interface AccountOverviewProps {
   account: Account
 }
 
-const AccountOverview: FC<AccountOverviewProps> = ({ account }) => {
-  const { data: transactions = undefined, loaded } = useTransactions(
-    account?.id
-  )
+const OverviewBalance: FC<AccountOverviewProps> = ({ account }) => {
   const [{ data: balance, loaded: balanceLoaded }, reloadBalance] =
     useAccountBalance(account?.id)
 
@@ -251,94 +251,104 @@ const AccountOverview: FC<AccountOverviewProps> = ({ account }) => {
   const navigate = useNavigate()
   const { loaded: synced } = useDataSync()
   return (
+    <Flex w="100%" pb="2rem">
+      <Box
+        layerStyle="card"
+        bg={`${accountGradientByOrder(account.order)} !important`}
+        borderRadius="0.25rem"
+        w="100%"
+        minWidth="18rem"
+        mr="1rem"
+        ml="0rem"
+        mt="0rem"
+      >
+        <Flex justifyContent="space-between" alignItems="center">
+          <Box m="2rem">
+            <Box>
+              <chakra.h4 color={NAMED_COLORS.DEEP_BLUE}>$IRON</chakra.h4>
+            </Box>
+            <Box mb="0.5rem">
+              <chakra.h2 color={NAMED_COLORS.DEEP_BLUE}>
+                {formatOreToTronWithLanguage(balance?.confirmed || BigInt(0))}
+              </chakra.h2>
+            </Box>
+            <Box>
+              <Button
+                variant="primary"
+                borderRadius="4rem"
+                mr="1rem"
+                borderColor="transparent"
+                leftIcon={
+                  <Icon height={8}>
+                    <SendIcon />
+                  </Icon>
+                }
+                onClick={() =>
+                  navigate(ROUTES.SEND, {
+                    state: { accountId: account.id },
+                  })
+                }
+                isDisabled={!synced}
+                disabled={!synced}
+              >
+                <h5>Send</h5>
+              </Button>
+              <Button
+                variant="primary"
+                borderRadius="4rem"
+                borderColor="transparent"
+                mr="1rem"
+                leftIcon={
+                  <Icon height={8}>
+                    <Receive />
+                  </Icon>
+                }
+                onClick={() =>
+                  navigate(ROUTES.RECEIVE, {
+                    state: { accountId: account.id },
+                  })
+                }
+                isDisabled={!synced}
+                disabled={!synced}
+              >
+                <h5>Receive</h5>
+              </Button>
+            </Box>
+          </Box>
+          <Box display={{ base: 'none', md: 'inline-block' }} m="1rem">
+            <FeesImage width={180} height={133} />
+          </Box>
+        </Flex>
+      </Box>
+      <Box
+        layerStyle="card"
+        p="2rem"
+        borderRadius="0.25rem"
+        minWidth="17.5rem"
+        mt="0rem"
+      >
+        <Box>
+          <chakra.h4>Pending $IRON</chakra.h4>
+        </Box>
+        <Box mb="0.5rem">
+          <chakra.h2>
+            {formatOreToTronWithLanguage(balance?.unconfirmed || BigInt(0))}
+          </chakra.h2>
+        </Box>
+      </Box>
+    </Flex>
+  )
+}
+
+const AccountOverview: FC<AccountOverviewProps> = ({ account }) => {
+  const { data: transactions = undefined, loaded } = useTransactions(
+    account?.id
+  )
+
+  return (
     <>
       <SyncWarningMessage mb="2rem" />
-      <Flex w="100%" pb="2rem">
-        <Box
-          layerStyle="card"
-          bg={`${accountGradientByOrder(account.order)} !important`}
-          borderRadius="0.25rem"
-          w="100%"
-          minWidth="18rem"
-          mr="1rem"
-          ml="0rem"
-          mt="0rem"
-        >
-          <Flex justifyContent="space-between" alignItems="center">
-            <Box m="2rem">
-              <Box>
-                <chakra.h4 color={NAMED_COLORS.DEEP_BLUE}>$IRON</chakra.h4>
-              </Box>
-              <Box mb="0.5rem">
-                <chakra.h2 color={NAMED_COLORS.DEEP_BLUE}>
-                  {formatOreToTronWithLanguage(balance?.confirmed || BigInt(0))}
-                </chakra.h2>
-              </Box>
-              <Box>
-                <Button
-                  variant="primary"
-                  borderRadius="4rem"
-                  mr="1rem"
-                  borderColor="transparent"
-                  leftIcon={
-                    <Icon height={8}>
-                      <SendIcon />
-                    </Icon>
-                  }
-                  onClick={() =>
-                    navigate(ROUTES.SEND, {
-                      state: { accountId: account.id },
-                    })
-                  }
-                  isDisabled={!synced}
-                  disabled={!synced}
-                >
-                  <h5>Send</h5>
-                </Button>
-                <Button
-                  variant="primary"
-                  borderRadius="4rem"
-                  borderColor="transparent"
-                  mr="1rem"
-                  leftIcon={
-                    <Icon height={8}>
-                      <Receive />
-                    </Icon>
-                  }
-                  onClick={() =>
-                    navigate(ROUTES.RECEIVE, {
-                      state: { accountId: account.id },
-                    })
-                  }
-                  isDisabled={!synced}
-                  disabled={!synced}
-                >
-                  <h5>Receive</h5>
-                </Button>
-              </Box>
-            </Box>
-            <Box display={{ base: 'none', md: 'inline-block' }} m="1rem">
-              <FeesImage width={180} height={133} />
-            </Box>
-          </Flex>
-        </Box>
-        <Box
-          layerStyle="card"
-          p="2rem"
-          borderRadius="0.25rem"
-          minWidth="17.5rem"
-          mt="0rem"
-        >
-          <Box>
-            <chakra.h4>Pending $IRON</chakra.h4>
-          </Box>
-          <Box mb="0.5rem">
-            <chakra.h2>
-              {formatOreToTronWithLanguage(balance?.unconfirmed || BigInt(0))}
-            </chakra.h2>
-          </Box>
-        </Box>
-      </Flex>
+      <OverviewBalance account={account} />
       <Box display={account.id && loaded ? 'block' : 'none'}>
         {transactions?.length === 0 ? (
           <EmptyOverview
