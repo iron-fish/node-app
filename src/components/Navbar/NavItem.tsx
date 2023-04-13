08154a2a41bc47eecd0311dec371c6eb0852e56c
Aliaksandr Zhurauski <zhurauski@scand.com>
2023-04-13 15:02:00 +0300

    use tooltip for navitems.

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Flex, NAMED_COLORS, useColorModeValue, Icon } from '@ironfish/ui-kit'
+import { Flex, NAMED_COLORS, Icon, Tooltip } from '@ironfish/ui-kit'
 import Hotkey from 'Components/Hotkey'
 
 export type NavItemProps = {
@@ -18,95 +18,69 @@ export const NavItem: FC<Omit<NavItemProps, 'aliases'>> = ({
   hotkey,
 }) => {
   const ItemIcon = icon as typeof Icon
-  const $colors = useColorModeValue(
-    {
-      borderRightColor: NAMED_COLORS.BLACK,
-      afterColor: NAMED_COLORS.WHITE,
-      afterBg: NAMED_COLORS.BLACK,
-    },
-    {
-      borderRightColor: NAMED_COLORS.WHITE,
-      afterColor: NAMED_COLORS.BLACK,
-      afterBg: NAMED_COLORS.WHITE,
-    }
-  )
   return (
     <>
-      <Flex
-        flexDirection="row"
-        justifyContent={{ base: 'center', sm: 'flex-start' }}
-        alignItems="center"
-        width={{ base: '3.5rem', sm: '14.5rem' }}
-        h="2.5rem"
-        borderRadius="0.25rem"
-        p={{ base: '0.5rem', sm: '0.5rem 0.5rem 0.5rem 1rem' }}
-        bg={active ? NAMED_COLORS.LIGHTER_GREY : 'transparent'}
+      <Tooltip
+        placement="right"
+        bg={NAMED_COLORS.DEEP_BLUE}
         _dark={{
-          bg: active ? NAMED_COLORS.DARK_GREY : 'transparent',
+          bg: NAMED_COLORS.WHITE,
+          '--popper-arrow-bg': NAMED_COLORS.WHITE,
         }}
-        _hover={{
-          '&>div': {
-            color: NAMED_COLORS.DEEP_BLUE,
-            _dark: {
-              color: NAMED_COLORS.WHITE,
-            },
-          },
-          '&::before': {
-            position: 'absolute',
-            left: '4.25rem',
-            content: '""',
-            width: 0,
-            height: 0,
-            border: '0.625rem solid transparent',
-            borderRightColor: $colors.borderRightColor,
-            display: { base: 'flex', sm: 'none' },
-          },
-          '&::after': {
-            content: `"${label}"`,
-            borderRadius: '0.25rem',
-            position: 'absolute',
-            justifyContent: 'center',
-            alignItems: 'center',
-            display: { base: 'flex', sm: 'none' },
-            left: '5.5rem',
-            bg: $colors.afterBg,
-            color: $colors.afterColor,
-            height: '2.5rem',
-            maxWidth: '8rem',
-            whiteSpace: 'nowrap',
-            padding: '0.5rem 0.75rem',
-            fontSize: '0.75rem',
-            fontFamily: 'favorit-regular',
-          },
-        }}
-        cursor="pointer"
+        boxShadow="none"
+        p={'0.5rem 0.75rem'}
+        m={0}
+        hasArrow={true}
+        label={label}
+        borderRadius="0.25rem"
+        height="2.5rem"
+        maxW={'8rem'}
+        display={{ base: 'flex', sm: 'none' }}
+        alignItems="center"
+        arrowSize={12.6}
+        offset={[0, 16]}
       >
-        <ItemIcon
-          fill={NAMED_COLORS.BLACK}
-          _dark={{
-            fill: NAMED_COLORS.WHITE,
-          }}
-          style={{ minWidth: '1.5rem' }}
-        />
         <Flex
           flexDirection="row"
-          paddingLeft="1rem"
-          fontSize="0.875rem"
-          color={active ? NAMED_COLORS.BLACK : NAMED_COLORS.GREY}
-          _dark={{
-            color: active ? NAMED_COLORS.WHITE : NAMED_COLORS.PALE_GREY,
-          }}
-          display={{ base: 'none', sm: 'flex' }}
-          whiteSpace="nowrap"
-          w="100%"
-          fontFamily="extended-regular"
+          justifyContent={{ base: 'center', sm: 'flex-start' }}
           alignItems="center"
-          justifyContent="space-between"
+          width={{ base: '3.5rem', sm: '14.5rem' }}
+          h="2.5rem"
+          borderRadius="0.25rem"
+          p={{ base: '0.5rem', sm: '0.5rem 0.5rem 0.5rem 1rem' }}
+          bg={active ? NAMED_COLORS.LIGHTER_GREY : 'transparent'}
+          _dark={{
+            bg: active ? NAMED_COLORS.DARK_GREY : 'transparent',
+          }}
+          cursor="pointer"
         >
-          {label}
-          {active && <Hotkey>{hotkey}</Hotkey>}
+          <ItemIcon
+            fill={NAMED_COLORS.BLACK}
+            _dark={{
+              fill: NAMED_COLORS.WHITE,
+            }}
+            style={{ minWidth: '1.5rem' }}
+          />
+          <Flex
+            flexDirection="row"
+            paddingLeft="1rem"
+            fontSize="0.875rem"
+            color={active ? NAMED_COLORS.BLACK : NAMED_COLORS.GREY}
+            _dark={{
+              color: active ? NAMED_COLORS.WHITE : NAMED_COLORS.PALE_GREY,
+            }}
+            display={{ base: 'none', sm: 'flex' }}
+            whiteSpace="nowrap"
+            w="100%"
+            fontFamily="extended-regular"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            {label}
+            {active && <Hotkey>{hotkey}</Hotkey>}
+          </Flex>
         </Flex>
-      </Flex>
+      </Tooltip>
     </>
   )
 }
