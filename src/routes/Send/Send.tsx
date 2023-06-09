48c5295ac0d5f37281a6136ecf680af52ad20dae
Evan Richard <evan@iflabs.network>
2023-06-09 14:13:28 -0400

    Update Send.tsx
    youe

--- src/routes/Send/Send.tsx
+++ src/routes/Send/Send.tsx
@@ -47,7 +47,7 @@ const Information: FC = memo(() => {
         _dark={{ color: NAMED_COLORS.LIGHT_GREY }}
       >
         You can change the fee amount you’d like to pay however that will
-        directly correlate the speed with which youe transaction is picked up by
+        directly correlate the speed with which your transaction is picked up by
         the block chain.
       </chakra.h5>
       <FeesImage mt="2rem" />
