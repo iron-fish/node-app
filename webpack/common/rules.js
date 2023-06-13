a85bb567a747b68b18826bcd0c54fa7645ac9478
Derek Guenther <dguenther9@gmail.com>
2023-06-13 15:46:21 -0400

    Fix migration ID and name loading in webpack

--- webpack/common/rules.js
+++ webpack/common/rules.js
@@ -1,4 +1,20 @@
 module.exports = [
+  {
+    test: /@ironfish\/sdk\/build\/src\/migrations\/data\/.*\.js$/,
+    loader: 'string-replace-loader',
+    options: {
+      search: '__filename',
+      replace(match, p1, offset, string) {
+        // Fixes migration files in the SDK relying on __filename to determine the ID and name of the migration.
+        // The migration filename is currently only used to extract the ID and name of the migration, not to read the
+        // actual file.
+        // After bundling, __filename will resolve to the webpack-bundled file's name,
+        // something like index.js, so we can avoid this by replacing __filename with the original filename.
+        return `'${this.resource.toString()}'`
+      },
+      flags: 'g',
+    },
+  },
   // Add support for native node modules
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
