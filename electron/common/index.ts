c37a3694df09d8509a04de4834e49fba5c14ff6f
Daniel Cortes <danielgcortes@gmail.com>
2023-06-28 10:00:27 -0600

    fix(Mac OS): Re-open window when cliking task bar icon

--- electron/common/index.ts
+++ electron/common/index.ts
@@ -94,6 +94,8 @@ app.on('ready', () => createWindow())
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', async () => {
+  mainWindow = null
+
   if (process.platform !== 'darwin') {
     app.quit()
   }
@@ -102,6 +104,7 @@ app.on('window-all-closed', async () => {
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
+
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
